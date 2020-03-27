123bd852c84e56afe685480c04c4f400e9ca6b6d
Benjamin Wichser <benwichser@gmail.com>
2020-03-27 19:01:55 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/[.?!]/)
+    sentences.select!(|i| i.length > 0)
+    binding.pry 
+    sentences.length 
   end
 end
